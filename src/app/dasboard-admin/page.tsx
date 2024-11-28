fd8289f811c5cc0c81bfb2de30f3a8dee465dbf5
BagusMiftahRizqullah <bagusmiftah9@gmail.com>
2024-11-28 16:23:13 +0800

    first commit

---
+++ src/app/dasboard-admin/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dashboard from '@/components/Dashboard/Dashboard'
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+function DasboardAdmin() {
+  return (
+    <div>
+    <DefaultLayout>
+         <Dashboard />
+    </DefaultLayout>
+    </div>
+  )
+}
+
+export default DasboardAdmin
